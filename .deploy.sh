bdc6a54731ebf31f6a94b08e5c4e367c91124947
wattanai <>
2020-05-11 07:46:31 +0700

    fix deploy

--- .deploy.sh
+++ .deploy.sh
@@ -1,6 +1,7 @@
 docker build -t peem16/multi-client:latest -t peem16/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t peem16/multi-server:latest -t peem16/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t peem16/multi-worker:latest -t peem16/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push peem16/multi-client:latest
 docker push peem16/multi-server:latest
 docker push peem16/multi-worker:latest
